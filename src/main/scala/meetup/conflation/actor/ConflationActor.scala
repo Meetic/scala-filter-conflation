d0eeb728699562652265d642d3f80042fc3062cd
c.verdier <c.verdier.ext@meetic-corp.com>
2016-11-23 16:23:22 +0100

    Implement monitoting recording in filter conflation actor

--- src/main/scala/meetup/conflation/actor/ConflationActor.scala
+++ src/main/scala/meetup/conflation/actor/ConflationActor.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import meetup.conflation.actor.ConflationActor.Tick
 import meetup.conflation.conflation.FilterConflation
 import meetup.conflation.event.SessionEvent
+import meetup.conflation.monitoring.InstrumentedFilterConflation
 
 import scala.collection.mutable
 
@@ -13,20 +14,28 @@ object ConflationActor {
   def props(target: ActorRef): Props = Props(new ConflationActor(target))
 }
 
-class ConflationActor(target: ActorRef) extends Actor with ActorLogging with FilterConflation[SessionEvent] {
+class ConflationActor(target: ActorRef) extends Actor with ActorLogging
+  with FilterConflation[SessionEvent] with InstrumentedFilterConflation {
 
   override val buffer = mutable.HashMap[String, Long]()
 
   override def sendOrFilterEvent(event: SessionEvent): Unit = {
+    recorder.recordIn()
     if (! buffer.contains(event.userId)) {
       target ! event
+      recorder.recordOut()
       buffer.put(event.userId, System.currentTimeMillis())
     }
-    // else ignore event
+    else {
+      // else ignore event
+      recorder.recordFiltered()
+    }
   }
 
   override def purgeFilteredEvents(): Unit = {
     log.info(s"Purging filtered events (${buffer.size})")
+    recorder.recordBufferSize(buffer.size)
+
     val threshold = System.currentTimeMillis() - delay.toMillis
     buffer.retain {
       case (_, time) if time < threshold => false
