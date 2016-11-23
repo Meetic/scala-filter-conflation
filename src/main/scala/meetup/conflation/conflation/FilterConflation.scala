71c3818c47f0758223f287b967853f7cc1185d03
c.verdier <c.verdier.ext@meetic-corp.com>
2016-11-23 16:43:23 +0100

    Comments on the FilterConflation interface, change the buffer type

--- src/main/scala/meetup/conflation/conflation/FilterConflation.scala
+++ src/main/scala/meetup/conflation/conflation/FilterConflation.scala
@@ -5,14 +5,19 @@ import scala.concurrent.duration._
 
 trait FilterConflation[E] {
 
+  // The delay during which messages will be conflated
   val delay = 60 seconds
+
+  // The interval at which the buffer will be checked for expired entries
   val tickInterval = 10 seconds
 
-  def buffer: mutable.HashMap[String, Conflated[E]]
+  // The buffer will hold the current conflation window for each providerId :
+  // providerId (String) is the key and time (Long) of the first event received is the value
+  def buffer: mutable.HashMap[String, Long]
 
+  // Method to check if the event's providerId is currently filtered and send it otherwise
   def sendOrFilterEvent(event: E): Unit
 
+  // Method to purge the buffer of expired entries
   def purgeFilteredEvents(): Unit
 }
-
-case class Conflated[E](event: E, time: Long)
