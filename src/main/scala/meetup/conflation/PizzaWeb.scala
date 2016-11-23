b05f8cdef58e87aa9242314d160e5c31a266d53a
c.verdier <c.verdier.ext@meetic-corp.com>
2016-11-23 16:22:34 +0100

    Add Kamon startup and shutdown

--- src/main/scala/meetup/conflation/PizzaWeb.scala
+++ src/main/scala/meetup/conflation/PizzaWeb.scala
@@ -1,11 +1,16 @@
 package meetup.conflation
 
 import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+import kamon.Kamon
 import meetup.conflation.actor.{ConflationActor, ProcessActor, SourceActor}
+
 import scala.concurrent.duration._
 
 object PizzaWeb extends App {
 
+  ConfigFactory.load()
+  Kamon.start()
   implicit val system = ActorSystem("pizza-web")
 
   val idRangeStart = 10000
@@ -17,4 +22,8 @@ object PizzaWeb extends App {
   val processActor = system.actorOf(ProcessActor.props(), "Process")
   val conflationActor = system.actorOf(ConflationActor.props(processActor), "Conflate")
   system.actorOf(SourceActor.props(conflationActor, idRangeStart, idRangeEnd, sourceDelay), "Source")
+
+  system.registerOnTermination {
+    Kamon.shutdown()
+  }
 }
